48937b026e19c82974278125a4c75647ec9f68c1
Ar-to <aricode0@gmail.com>
2017-05-02 22:41:14 -0700

    updated to allow npm scripts to build for dev, production and to use browsersync and http-server

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,18 @@ var sourcemaps = require('gulp-sourcemaps');//using
 var requirejsOptimize = require('gulp-requirejs-optimize');//using;optimizes modules individually & bundle (minify)
 var path = require("path");//using for callback for pug task
 var browserSync = require('browser-sync').create();
+//build packages
+var useref = require('gulp-useref');//https://www.npmjs.com/package/gulp-useref
+var uglify = require('gulp-uglify');//used only as needed for dist js vendor files; https://www.npmjs.com/package/gulp-uglify
+var gulpIf = require('gulp-if');//https://github.com/robrich/gulp-if
+var cssnano = require('gulp-cssnano');//https://www.npmjs.com/package/gulp-cssnano
+var imagemin = require('gulp-imagemin');//https://www.npmjs.com/package/gulp-imagemin
+var cache = require('gulp-cache');//to help speed up image optimization; https://www.npmjs.com/package/gulp-cache
+//cleaning
+var del = require('del');//delete files/directories w/globs; https://www.npmjs.com/package/del
+//build
+var runSequence = require('run-sequence');//run tasks in order; https://www.npmjs.com/package/run-sequence
+
 
 var paths = {
   sass: ['./src/sass/**/*.sass'],
@@ -66,8 +78,8 @@ gulp.task('requireopt', function () {
         //used for requirejs module loading; needed for shim see:http://www.requirejs.org/docs/optimization.html#mainConfigFile
         mainConfigFile: './src/js/require.config.js',//relative to gulpfile
         paths: {
-          nav: 'modules/navmod',//relative to baseUrl
-          smoothscll: 'modules/smoothscroll',
+          nav: 'modules/one_module',//relative to baseUrl
+          smoothscll: 'modules/two_module',
           nav_color: 'modules/nav_color'
         },
         include: ['../require.config'],//relative to baseUrl
@@ -78,16 +90,11 @@ gulp.task('requireopt', function () {
     .pipe(sourcemaps.write('../sourcemaps'))//writes sourcemaps to directory:relative to gulp.dest()!!; comment to remove
     .pipe(gulp.dest('./dist/js'));//output directory
 });
-//$ gulp default works
-gulp.task('default', ['sass', 'pug', 'requireopt']);
 //$ gulp watch works
 gulp.task('watch', ['sass', 'pug', 'requireopt'], function() {
   gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.pugPartials, ['pug']);
   gulp.watch(paths.pugMain, ['pug']);
-  //saves only changes in require.config.js
-  //gulp.watch('./src/js/require.config.js', ['requireopt']);
-  //works: takes all changes with *.js and updates the bundle.js
   gulp.watch(paths.require, ['requireopt']);
 });
 gulp.task('watchsass', ['sass'], function() {
@@ -102,7 +109,7 @@ gulp.task('watchjs', ['requireopt'], function() {
   gulp.watch(paths.require, ['requireopt']);
 });
 //not used unless folders are desired for compiled *.html files
-function callback(file) {
+/*function callback(file) {//not being used!!
   if (file.path.search('index') !== -1) {//if path is index then return to directory
     //return './www/app/';
     return './dist/templates/';//excluded index.html goes here
@@ -110,18 +117,31 @@ function callback(file) {
   var folder = path.basename(file.path).replace(/\..*html/, '/');
   //return './www/app/' + folder;
   return './dist/templates/' + folder;//used to organize compiled *.html into folders
-}
-
+}*/
 //copy global files to dist
+gulp.task('css-files', function() {
+  gulp.src('./src/css/*.css')
+  .pipe(gulp.dest('./dist/css'));
+});
+
 gulp.task('js-files', function() {
-  gulp.src('./temp/*.js')
+  gulp.src(['./src/js/*.js', '!./src/js/require.config.js'])
   .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('png-files', function() {
-  gulp.src('./temp/*.png')
+gulp.task('img-files', function() {
+  gulp.src('./src/images/**/*.+(png|jpg|gif|svg)')
   .pipe(gulp.dest('./dist/images'));
 });
+
+gulp.task('copy-all', function (callback) {
+  runSequence(
+    'css-files',
+    'js-files',
+    'img-files',
+    callback
+  )
+})
 
 // Static server
 gulp.task('browser-sync', function() {
@@ -134,8 +154,45 @@ gulp.task('browser-sync', function() {
     });
 });
 
-// Static server for changes to dist
-gulp.task('serve', ['browser-sync', 'js-files', 'png-files'], function() {
+//tasks for optimization/production
+//based on https://css-tricks.com/gulp-for-beginners/
+
+//useref for concatenating js/css files from different directories
+gulp.task('useref', function(){
+  return gulp.src('dist/index.html')
+    .pipe(useref())
+    // Minifies only if it's a JavaScript file
+    .pipe(gulpIf('*.js', uglify()))
+    // Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('public'))
+});
+//optimize images w/options for each type plus cache to speed up process
+gulp.task('images', function(){
+  return gulp.src('dist/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(cache(imagemin()))
+  .pipe(gulp.dest('public/images'))
+});
+//clear cache
+gulp.task('cache:clear', function (done) {
+return cache.clearAll(done)
+})
+//clean: delete directory when not in use
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+})
+gulp.task('clean:public', function() {
+  return del.sync('public');
+})
+
+//===Build tasks
+
+//$ gulp ; creates dist folder with all files + global copies
+gulp.task('default', ['sass', 'pug', 'requireopt', 'copy-all']);
+
+//Run server to auto update browser upon file changes
+// Static server for changes to dist, copies global files & reloads browser when compiled files are changed
+gulp.task('serve', ['browser-sync', 'css-files', 'js-files', 'img-files'], function() {
   gulp.watch(paths.sass, ['sass']);//sass not compiling and browser not reloading
   gulp.watch(paths.pugPartials, ['pug']);
   gulp.watch(paths.pugMain, ['pug']);
@@ -144,17 +201,19 @@ gulp.task('serve', ['browser-sync', 'js-files', 'png-files'], function() {
   gulp.watch(paths.css, browserSync.reload);
   gulp.watch(paths.js, browserSync.reload);
 });
-//===Build tasks
-//works: minifyJS and uses pump to handle error similar to a sourcemap but for gulp to compensate for unclear pipe errors
-//bundle.js is already minified but additional files may need it
-gulp.task('compress', function (cb) {
-  pump([
-        gulp.src('./dist/js/*.js'),//change directory for specific js files to minify
-        uglify(),
-        gulp.dest('./dist/minify')
-    ],
-    cb
-  );
-});
-//build for /public
-//gulp.task('build', ['sass', 'pug', 'requireopt']);
+
+//build public folder
+gulp.task('public', ['useref', 'images']);
+
+//clean by deleting dist, public and clearing cache
+gulp.task('clean-all', ['cache:clear', 'clean:dist', 'clean:public']);
+
+//builds for production the public folder
+gulp.task('build', function (callback) {
+  runSequence(
+    ['clean:dist', 'clean:public'],
+    'default',
+    'public',
+    callback
+  )
+})
